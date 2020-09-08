be9baf89466cf62189b5bc60d93df872de64f30f
Alex Kehayias <alex.kehayias@gmail.com>
2020-09-07 17:13:31 -0700

    Use a shared state struct for data shared between FSM states
    This avoids needing to manually add attributes to each state that are
    common between states.

--- src/gui.rs
+++ src/gui.rs
@@ -2,42 +2,71 @@
 //! states.
 use crate::engine::{Target, EntityId};
 
+/// A collection of shared state between different transitions. Useful
+/// so you don't need to duplicate the same attributes across multiple
+/// states.
+pub struct SharedState {
+    enemy_id: EntityId,
+    target_id: Option<EntityId>,
+}
 
-struct GuiStateMachine<T> {
-    state: T
+pub struct GuiStateMachine<T> {
+    pub state: T
 }
 
 impl GuiStateMachine<Combat> {
-    fn new(enemy_id: EntityId) -> Self {
+    pub fn new(enemy_id: EntityId) -> Self {
         GuiStateMachine {
             state: Combat {
-                enemy_id,
-                target_id: None
+                shared_state: SharedState {
+                    enemy_id,
+                    target_id: None
+                }
             }
         }
     }
 }
 
-struct Combat {
-    enemy_id: EntityId,
-    target_id: Option<EntityId>
+pub struct Combat {
+    shared_state: SharedState,
 }
 
-struct Targeting {
-    enemy_id: EntityId,
+pub struct Targeting {
+    shared_state: SharedState,
     target_type: Target,
     targets: Vec<EntityId>,
-    target_id: Option<EntityId>
 }
 
 impl From<GuiStateMachine<Combat>> for GuiStateMachine<Targeting> {
     fn from(val: GuiStateMachine<Combat>) -> GuiStateMachine<Targeting> {
+        let targets = vec![val.state.shared_state.enemy_id];
         GuiStateMachine {
             state: Targeting {
-                enemy_id: val.state.enemy_id,
+                shared_state: val.state.shared_state,
                 target_type: Target::Single,
-                targets: vec![val.state.enemy_id],
-                target_id: None
+                targets: targets,
+            }
+        }
+    }
+}
+
+impl From<&GuiStateMachine<Combat>> for GuiStateMachine<Targeting> {
+    fn from(val: &GuiStateMachine<Combat>) -> GuiStateMachine<Targeting> {
+        let enemy_id = val.state.shared_state.enemy_id;
+        let target_id = val.state.shared_state.target_id;
+        let targets = vec![enemy_id];
+
+        GuiStateMachine {
+            state: Targeting {
+                // HACK: Constructing the shared state manually to
+                // avoid a borrowck error because we're using
+                // state.shared_state to create the vector of targets
+                shared_state: SharedState {
+                    enemy_id,
+                    target_id
+                },
+                target_type: Target::Single,
+                targets: targets
             }
         }
     }
@@ -45,10 +74,15 @@ impl From<GuiStateMachine<Combat>> for GuiStateMachine<Targeting> {
 
 impl From<GuiStateMachine<Targeting>> for GuiStateMachine<Combat> {
     fn from(val: GuiStateMachine<Targeting>) -> GuiStateMachine<Combat> {
+
+        // TODO Is there a way to make this not a runtime exception?
+        // Assert that we have a target ID otherwise this is an
+        // invalid transition.
+        val.state.shared_state.target_id.expect("A target entity ID should have been selected at this point");
+
         GuiStateMachine {
             state: Combat {
-                enemy_id: val.state.enemy_id,
-                target_id: Some(val.state.target_id.expect("A target entity ID should have been selected at this point")),
+                shared_state: val.state.shared_state
             }
         }
     }
@@ -64,11 +98,11 @@ mod test_gui_state_machine {
 
         // Simulate the user selecting a target
         let mut targeting_state = GuiStateMachine::<Targeting>::from(combat_state);
-        targeting_state.state.target_id = Some(enemy_id);
+        targeting_state.state.shared_state.target_id = Some(enemy_id);
         let combat_state_with_target = GuiStateMachine::<Combat>::from(targeting_state);
 
         assert_eq!(
-            combat_state_with_target.state.target_id.unwrap(),
+            combat_state_with_target.state.shared_state.target_id.unwrap(),
             enemy_id
         );
     }
